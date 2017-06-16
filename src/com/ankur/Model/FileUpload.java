1d31668bf755814d8a1559d4b84ae22f7c9b3d38
ankur kumar <ankur kumar>
2017-06-16 22:43:16 +0530

    adding the modified files

--- src/com/ankur/Model/FileUpload.java
+++ src/com/ankur/Model/FileUpload.java
@@ -1,12 +1,60 @@
-package com.ankur.controller;
+package com.ankur.Model;
 
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 import org.springframework.web.multipart.MultipartFile;
 
+@Entity
+@Table(name = "File_Details")
 public class FileUpload {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "Image_Id")
+	private Long fileId;
+
+	@Column(name = "File_Name")
+	private String fileName;
+
+	@Column(name = "fileData")
+	@Lob
+	private byte[] fileData;
+
+	@Transient
 	private List<MultipartFile> files;
+
+	public Long getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(Long fileId) {
+		this.fileId = fileId;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public byte[] getFileData() {
+		return fileData;
+	}
+
+	public void setFileData(byte[] fileData) {
+		this.fileData = fileData;
+	}
 
 	public List<MultipartFile> getFiles() {
 		return files;
--- src/com/ankur/controller/UploadController.java
+++ src/com/ankur/controller/UploadController.java
@@ -1,6 +1,5 @@
 package com.ankur.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,6 +9,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,8 +20,14 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ankur.Dao.ImageUploadDao;
+import com.ankur.Model.FileUpload;
+
 @Controller
 public class UploadController implements HandlerExceptionResolver {
+
+	@Autowired
+	private ImageUploadDao imageUploadDao;
 
 	@RequestMapping(value = "/upload", method = RequestMethod.GET)
 	public ModelAndView displayForm() {
@@ -34,9 +40,10 @@ public class UploadController implements HandlerExceptionResolver {
 	public String saveFile(@ModelAttribute("uploadFile") FileUpload fileUpload, Model map)
 			throws IllegalStateException, IOException {
 
-		String saveDirectory = "c:/Spring/";
-
+		System.out.println("I am here in the code");
 		List<MultipartFile> files = fileUpload.getFiles();
+
+		System.out.println("Number o files received :" + files.size());
 
 		List<String> fileNames = new ArrayList<String>();
 
@@ -44,11 +51,23 @@ public class UploadController implements HandlerExceptionResolver {
 			for (MultipartFile multipartFile : files) {
 
 				String fileName = multipartFile.getOriginalFilename();
-				if (!"".equalsIgnoreCase(fileName)) {
-					// Handle file content - multipartFile.getInputStream()
-					multipartFile.transferTo(new File(saveDirectory + fileName));
-					fileNames.add(fileName);
-				}
+
+				System.out.println("Name of file is :" + fileName);
+				byte[] data = multipartFile.getBytes();
+
+				FileUpload upload = new FileUpload();
+				upload.setFileName(fileName);
+				upload.setFileData(data);
+				// if (!"".equalsIgnoreCase(fileName)) {
+				// // Handle file content - multipartFile.getInputStream()
+				// multipartFile.transferTo(new File(saveDirectory + fileName));
+				// fileNames.add(fileName);
+				// }
+
+				System.out.println("Now images will be saved");
+				imageUploadDao.save(upload);
+				System.out.println("Image saved in the DB");
+				fileNames.add(fileName);
 			}
 		}
 
